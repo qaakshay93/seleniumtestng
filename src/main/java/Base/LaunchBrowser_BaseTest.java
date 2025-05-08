e446d63a557f23a869f840bfd6b79e538e06bef8
qaakshay93 <akshayshirkeab@gmail.com>
2025-05-08 20:58:40 +0530

    new browsers added

--- src/main/java/Base/LaunchBrowser_BaseTest.java
+++ src/main/java/Base/LaunchBrowser_BaseTest.java
@@ -2,18 +2,51 @@ package Base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 
 public class LaunchBrowser_BaseTest {
 
 	protected WebDriver driver;
 
 	@BeforeMethod
+	@Parameters("browser")
 
-	public void browsersetup() {
+	public void browsersetup(String browser) {
 
-		driver = new ChromeDriver();
+		String Projectdire = System.getProperty("user.dir");
+		System.out.println("Project path is = : " + Projectdire);
+
+		if (browser.equalsIgnoreCase("chrome")) 
+		{
+
+			System.setProperty("webdriver.chrome.driver", Projectdire + "/Driver/chromedriver.exe");
+			driver = new ChromeDriver();
+
+
+		}
+		else if(browser.equalsIgnoreCase("firefox"))
+		{
+			System.setProperty("webdriver.gecko.driver", Projectdire + "/Driver/geckodriver.exe");
+			driver = new FirefoxDriver();
+	
+		}
+		
+		else if(browser.equalsIgnoreCase("Edge"))
+		{
+			System.setProperty("webdriver.edge.driver", Projectdire + "/Driver/msedgedriver.exe");
+			driver = new EdgeDriver();
+
+		}
+		else 
+		{
+			throw new IllegalArgumentException("Browser not supported : " + browser);
+			
+		}
+		
 		driver.manage().window().maximize();
 		driver.get("https://admin-demo.nopcommerce.com/admin");
 		driver.navigate().refresh();
